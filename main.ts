b2a8bb3b1e718bfe5e5b839e7318db7d76e56de6
ntopia <ntopia.p@gmail.com>
2017-10-06 20:32:04 +0900

    간단한 선 그려보기

--- main.ts
+++ main.ts
@@ -2,9 +2,20 @@
 import * as PIXI from 'pixi.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    let renderer = PIXI.autoDetectRenderer(600, 400,
+    let app = new PIXI.Application(800, 600,
         {
-            antialias: true, transparent: false, resolution: 1, backgroundColor: 0xFFFFFF
+            antialias: true, transparent: false, resolution: 1, backgroundColor: 0xcccccc
         }
     );
+    document.body.appendChild(app.view);
+
+    let stage = new PIXI.Container();
+
+    let line = new PIXI.Graphics();
+    line.lineStyle(3, 0, 1);
+    line.moveTo(1, 1);
+    line.lineTo(200, 200);
+    stage.addChild(line);
+
+    app.stage.addChild(stage);
 });
