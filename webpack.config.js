22e32e82d0eba97b21f059eb3130d062ad21eaf0
ntopia <ntopia.p@gmail.com>
2017-10-06 19:21:16 +0900

    webpack 세팅함

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: './main.ts',
+    output: {
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ }
+        ]
+    }
+}
